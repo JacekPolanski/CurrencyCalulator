a0a80c1dcb6a43d3f9c13ff7cdd3bd573c7a450a
Jacek Polański <kontakt@jpolanski.pl>
2015-08-12 21:59:59 +0200

    tests - refactoring

--- tests.py
+++ tests.py
@@ -7,56 +7,42 @@ from currency_calculator import CurrencyCalculator
 
 class TestCurrencyCalculator(unittest.TestCase):
 
+    __exchange_rate = 4.2
+
     def setUp(self):
         self.calculator = CurrencyCalculator()
 
-    def test_currency_calculator_has_euro_exchange_rate(self):
-        assert self.calculator.euro_exchange_rate != None, \
-            "currency_calculator euro_exchange_rate is None"
+    def test_calculator_has_exchange_rates(self):
+        assert self.calculator.euro_exchange_rate is not None, "euro_exchange_rate is None"
+        assert self.calculator.pound_exchange_rate is not None, "pound_exchange_rate is None"
+        assert self.calculator.dollar_exchange_rate is not None, "dollar_exchange_rate is None"
 
-    def test_currency_calculator_has_pound_exchange_rate(self):
-        assert self.calculator.pound_exchange_rate != None, \
-            "currency_calculator pound_exchange_rate is None"
+    def test_calculator_set_exchange_rates(self):
+        self.calculator.set_euro_exchange_rate(self.__exchange_rate)
+        self.calculator.set_pound_exchange_rate(self.__exchange_rate)
+        self.calculator.set_dollar_exchange_rate(self.__exchange_rate)
 
-    def test_currency_calculator_has_dollar_exchange_rate(self):
-        assert self.calculator.dollar_exchange_rate != None, \
-            "currency_calculator dollar_exchange_rate is None"
+        assert self.calculator.euro_exchange_rate == self.__exchange_rate, "euro_exchange_rate did not set"
+        assert self.calculator.pound_exchange_rate == self.__exchange_rate, "pound_exchange_rate did not set"
+        assert self.calculator.dollar_exchange_rate == self.__exchange_rate, "dollar_exchange_rate did not set"
 
-    def test_currency_calculator_set_euro_exchange_rate(self):
-        self.calculator.set_euro_exchange_rate(4.1)
-        assert self.calculator.euro_exchange_rate == 4.1, \
-            "currency_calculator euro_exchange_rate did not set"
+    def test_calculator_converts_to_pln(self):
+        self.calculator.set_euro_exchange_rate(self.__exchange_rate)
+        self.calculator.set_pound_exchange_rate(self.__exchange_rate)
+        self.calculator.set_dollar_exchange_rate(self.__exchange_rate)
 
-    def test_currency_calculator_set_pound_exchange_rate(self):
-        self.calculator.set_pound_exchange_rate(4.6)
-        assert self.calculator.pound_exchange_rate == 4.6, \
-            "currency_calculator pound_exchange_rate did not set"
+        assert self.calculator.convert_euro_to_pln(1) == self.__exchange_rate, "conversion euro to pln failed"
+        assert self.calculator.convert_pound_to_pln(1) == self.__exchange_rate, "conversion pound to pln failed"
+        assert self.calculator.convert_dollar_to_pln(1) == self.__exchange_rate, "conversion dollar to pln failed"
 
-    def test_currency_calculator_set_dollar_exchange_rate(self):
-        self.calculator.set_dollar_exchange_rate(3.7)
-        assert self.calculator.dollar_exchange_rate == 3.7, \
-            "currency_calculator dollar_exchange_rate did not set"
+        assert self.calculator.convert_euro_to_pln(3) == 3*self.__exchange_rate, "conversion euro to pln failed"
+        assert self.calculator.convert_pound_to_pln(3) == 3*self.__exchange_rate, "conversion pound to pln failed"
+        assert self.calculator.convert_dollar_to_pln(3) == 3*self.__exchange_rate, "conversion dollar to pln failed"
 
-    def test_currency_calculator_convert_euro_to_pln(self):
-        self.calculator.set_euro_exchange_rate(4.1)
-        assert self.calculator.convert_euro_to_pln(1) == 4.1, \
-            "currency_calculator convert euro to pln failed"
-        assert self.calculator.convert_euro_to_pln(3) == 3*4.1, \
-            "currency_calculator convert euro to pln failed"
+        assert self.calculator.convert_euro_to_pln(3.4) == 3.4*self.__exchange_rate, "conversion euro to pln failed"
+        assert self.calculator.convert_pound_to_pln(3.4) == 3.4*self.__exchange_rate, "conversion pound to pln failed"
+        assert self.calculator.convert_dollar_to_pln(3.4) == 3.4*self.__exchange_rate, "conversion dollar to pln failed"
 
-    def test_currency_calculator_convert_pound_to_pln(self):
-        self.calculator.set_pound_exchange_rate(4.6)
-        assert self.calculator.convert_pound_to_pln(1) == 4.6, \
-            "currency_calculator convert pound to pln failed"
-        assert self.calculator.convert_pound_to_pln(3) == 3*4.6, \
-            "currency_calculator convert pound to pln failed"
-
-    def test_currency_calculator_convert_dollar_to_pln(self):
-        self.calculator.set_dollar_exchange_rate(4.6)
-        assert self.calculator.convert_dollar_to_pln(1) == 4.6, \
-            "currency_calculator convert dollar to pln failed"
-        assert self.calculator.convert_dollar_to_pln(3) == 3*4.6, \
-            "currency_calculator convert dollar to pln failed"
 
 if __name__=="__main__":
     unittest.main()
