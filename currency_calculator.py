9c6d37038778d8d55e1331143c8940dc4ad703c8
Jacek Polański <kontakt@jpolanski.pl>
2015-08-12 21:36:49 +0200

    added dollar calculator

--- currency_calculator.py
+++ currency_calculator.py
@@ -5,6 +5,7 @@ class CurrencyCalculator:
     def __init__(self):
         self.euro_exchange_rate = 0
         self.pound_exchange_rate = 0
+        self.dollar_exchange_rate = 0
 
     def set_euro_exchange_rate(self, value):
         self.euro_exchange_rate = value
@@ -12,9 +13,14 @@ class CurrencyCalculator:
     def set_pound_exchange_rate(self, value):
         self.pound_exchange_rate = value
 
+    def set_dollar_exchange_rate(self, value):
+        self.dollar_exchange_rate = value
+
     def convert_euro_to_pln(self, amount):
         return self.euro_exchange_rate*amount
 
     def convert_pound_to_pln(self, amount):
         return self.pound_exchange_rate*amount
 
+    def convert_dollar_to_pln(self, amount):
+        return self.dollar_exchange_rate*amount
--- tests.py
+++ tests.py
@@ -18,6 +18,10 @@ class TestCurrencyCalculator(unittest.TestCase):
         assert self.calculator.pound_exchange_rate != None, \
             "currency_calculator pound_exchange_rate is None"
 
+    def test_currency_calculator_has_dollar_exchange_rate(self):
+        assert self.calculator.dollar_exchange_rate != None, \
+            "currency_calculator dollar_exchange_rate is None"
+
     def test_currency_calculator_set_euro_exchange_rate(self):
         self.calculator.set_euro_exchange_rate(4.1)
         assert self.calculator.euro_exchange_rate == 4.1, \
@@ -27,6 +31,11 @@ class TestCurrencyCalculator(unittest.TestCase):
         self.calculator.set_pound_exchange_rate(4.6)
         assert self.calculator.pound_exchange_rate == 4.6, \
             "currency_calculator pound_exchange_rate did not set"
+
+    def test_currency_calculator_set_dollar_exchange_rate(self):
+        self.calculator.set_dollar_exchange_rate(3.7)
+        assert self.calculator.dollar_exchange_rate == 3.7, \
+            "currency_calculator dollar_exchange_rate did not set"
 
     def test_currency_calculator_convert_euro_to_pln(self):
         self.calculator.set_euro_exchange_rate(4.1)
@@ -38,9 +47,16 @@ class TestCurrencyCalculator(unittest.TestCase):
     def test_currency_calculator_convert_pound_to_pln(self):
         self.calculator.set_pound_exchange_rate(4.6)
         assert self.calculator.convert_pound_to_pln(1) == 4.6, \
-            "currency_calculator convert euro to pln failed"
+            "currency_calculator convert pound to pln failed"
         assert self.calculator.convert_pound_to_pln(3) == 3*4.6, \
-            "currency_calculator convert euro to pln failed"
+            "currency_calculator convert pound to pln failed"
+
+    def test_currency_calculator_convert_dollar_to_pln(self):
+        self.calculator.set_dollar_exchange_rate(4.6)
+        assert self.calculator.convert_dollar_to_pln(1) == 4.6, \
+            "currency_calculator convert dollar to pln failed"
+        assert self.calculator.convert_dollar_to_pln(3) == 3*4.6, \
+            "currency_calculator convert dollar to pln failed"
 
 if __name__=="__main__":
     unittest.main()
